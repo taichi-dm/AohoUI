f0407c4a24de0e029a7c2bf3ee9635675d33d6e9
taichi <positive703@gmail.com>
2021-09-30 03:38:21 +0900

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# AohoUI
+
+A description of this package.
---
+++ Tests/AohoUITests/AohoUITests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import AohoUI
+
+final class AohoUITests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(AohoUI().text, "Hello, World!")
+    }
+}

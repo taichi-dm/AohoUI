c7732380d3098abe787e6c41ced0448ff8727770
taichi <positive703@gmail.com>
2021-09-30 16:01:32 +0900

    [feat] four new color set is published.

--- Sources/AohoUI/AohoUI.swift
+++ Sources/AohoUI/AohoUI.swift
@@ -7,6 +7,10 @@ public struct AohoUI {
     public struct Colors {
         public static let shadowColor: Color = Color.secondary.opacity(0.05)
         public static let aoho_green: Color = Color.aoho_green
+        public static let aoho_primary_label: Color = Color.aoho_primary_label
+        public static let aoho_secondary_label: Color = Color.aoho_secondary_label
+        public static let aoho_tertiary_label: Color = Color.aoho_tertiary_label
+        public static let aoho_quaternary_label: Color = Color.aoho_quaternary_label
     }
     
     public struct Token {

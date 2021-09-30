0dd0e1585e1ea73239d208d3794fa1712cf44a4e
taichi <positive703@gmail.com>
2021-09-30 10:18:18 +0900

    [feat] AohoUI has been arrived to our project!

--- Sources/AohoUI/AohoUI.swift
+++ Sources/AohoUI/AohoUI.swift
@@ -1,6 +1,14 @@
 import UIKit
 import SwiftUI
 
+public struct AohoUI {
+    // 20
+    static let cornerRadius: CGFloat = 20.0
+    
+    @available(iOS 13.0, *)
+    static let shadowColor: Color = Color.secondary.opacity(0.05)
+}
+
 public extension UIColor {
     @available(iOS 11.0, *)
     static let aoho_green: UIColor = UIColor(named: "aoho_green")!

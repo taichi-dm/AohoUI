266f5f35c16ff6c46f3a8c1299e070d244231fb5
taichi <positive703@gmail.com>
2021-09-30 13:50:59 +0900

    [refactor] remove unnecessary lines.

--- Sources/AohoUI/AohoUI.swift
+++ Sources/AohoUI/AohoUI.swift
@@ -1,24 +1,22 @@
+
 import UIKit
 import SwiftUI
 
-@available(iOS 13.0, *)
 public struct AohoUI {
     // 20
-    static let cornerRadius: CGFloat = 20.0
-    static let spacing: CGFloat = 15
+    public static let cornerRadius: CGFloat = 20.0
+    public static let spacing: CGFloat = 15
     
     
-    static let shadowColor: Color = Color.secondary.opacity(0.05)
-    static let aoho_green: Color = Color.aoho_green
+    public static let shadowColor: Color = Color.secondary.opacity(0.05)
+    public static let aoho_green: Color = Color.aoho_green
 }
 
-@available(iOS 13.0, *)
 private extension Color {
     static let aoho_green: Color = Color("aoho_green")
 }
 
 
-@available(iOS 13.0.0, *)
 public struct AHPrimaryButtonStyle: ButtonStyle {
     public func makeBody(configuration: Configuration) -> some View {
         configuration.label
@@ -29,7 +27,6 @@ public struct AHPrimaryButtonStyle: ButtonStyle {
     }
 }
 
-@available(iOS 13.0.0, *)
 public struct AHSecondaryButtonStyle: ButtonStyle {
     public func makeBody(configuration: Configuration) -> some View {
         configuration.label

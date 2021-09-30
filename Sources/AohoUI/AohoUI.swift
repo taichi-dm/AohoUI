162ad1708ffd8a6991621d230bcaac760e10a33e
taichi <positive703@gmail.com>
2021-09-30 10:14:00 +0900

    First Commit

--- Sources/AohoUI/AohoUI.swift
+++ Sources/AohoUI/AohoUI.swift
@@ -1,6 +1,12 @@
 import UIKit
+import SwiftUI
 
 public extension UIColor {
     @available(iOS 11.0, *)
     static let aoho_green: UIColor = UIColor(named: "aoho_green")!
+}
+
+@available(iOS 13.0, *)
+public extension Color {
+    static let aoho_green: Color = Color("aoho_green")
 }

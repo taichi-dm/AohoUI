c46d3320d8880dfe5ad5f7c6c8356fbcc67bb84b
taichi <positive703@gmail.com>
2021-09-30 13:52:20 +0900

    [feat] My First Test!

--- Tests/AohoUITests/AohoUITests.swift
+++ Tests/AohoUITests/AohoUITests.swift
@@ -1,11 +1,13 @@
 import XCTest
-@testable import AohoUI
+import AohoUI
+import SwiftUI
 
 final class AohoUITests: XCTestCase {
     func testExample() throws {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-        XCTAssertEqual(AohoUI().text, "Hello, World!")
+        XCTAssertEqual(AohoUI.aoho_green, Color("aoho_green"))
+        XCTAssertEqual(AohoUI.cornerRadius, 20.0)
     }
 }

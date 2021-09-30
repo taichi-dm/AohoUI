d3c0fb2c6c45d4b412e80abb168cd9f56dd2facb
taichi <positive703@gmail.com>
2021-09-30 10:33:31 +0900

    [feat] Added AHPrimaryButtonStyle and AHSecondaryButtonStyle those are used with SwiftUI Button.

--- Sources/AohoUI/AohoUI.swift
+++ Sources/AohoUI/AohoUI.swift
@@ -1,20 +1,37 @@
 import UIKit
 import SwiftUI
 
+@available(iOS 13.0, *)
 public struct AohoUI {
     // 20
     static let cornerRadius: CGFloat = 20.0
+    static let spacing: CGFloat = 15
     
-    @available(iOS 13.0, *)
+    
     static let shadowColor: Color = Color.secondary.opacity(0.05)
-}
-
-public extension UIColor {
-    @available(iOS 11.0, *)
-    static let aoho_green: UIColor = UIColor(named: "aoho_green")!
+    static let aoho_green: Color = Color.aoho_green
 }
 
 @available(iOS 13.0, *)
-public extension Color {
+private extension Color {
     static let aoho_green: Color = Color("aoho_green")
+}
+
+
+@available(iOS 13.0.0, *)
+struct AHPrimaryButtonStyle: ButtonStyle {
+    func makeBody(configuration: Configuration) -> some View {
+        configuration.label
+            .frame(maxWidth: .infinity)
+            .padding()
+            .background(Capsule().fill(Color.white))
+            .padding(.vertical)
+    }
+}
+
+@available(iOS 13.0.0, *)
+struct AHSecondaryButtonStyle: ButtonStyle {
+    func makeBody(configuration: Configuration) -> some View {
+        configuration.label
+    }
 }
